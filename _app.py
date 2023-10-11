106d5fe11b50ff201314feefb44823f16d6db3c7
mustufabaig <mustufa.baig@gmail.com>
2023-10-11 10:26:29 -0400

    Update _app.py

--- _app.py
+++ _app.py
@@ -2,11 +2,32 @@ import streamlit as st
 import json
 
 from langchain.chat_models import AzureChatOpenAI
-from langchain_experimental.sql import SQLDatabaseChain
-from langchain.prompts.prompt import PromptTemplate
-from langchain import FewShotPromptTemplate
+from langchain import PromptTemplate
 
-import fewshotprompttemplate
+template = """Answer the question based on the context below. If the
+question cannot be answered using the information provided answer
+with "I don't know".
+
+Context: Large Language Models (LLMs) are the latest models used in NLP.
+Their superior performance over smaller models has made them incredibly
+useful for developers building NLP enabled applications. These models
+can be accessed via Hugging Face's `transformers` library, via OpenAI
+using the `openai` library, and via Cohere using the `cohere` library.
+
+Question: {query}
+
+Answer: """
+
+prompt_template = PromptTemplate(
+    input_variables=["query"],
+    template=template
+)
+
+st.code(
+    prompt_template.format(
+        query="Which libraries and model providers offer LLMs?"
+    )
+)
 
 def get_chain():
     if 'chain' not in st.session_state:
@@ -22,16 +43,7 @@ def get_chain():
         llm = AzureChatOpenAI(temperature=0, deployment_name=OPENAI_CHAT_MODEL, model='gpt-4', verbose=True)
         
         #prompt template
-        # now create the few shot prompt template
-        few_shot_prompt_template = FewShotPromptTemplate(
-            examples=fewshotprompttemplate.examples,
-            example_prompt=fewshotprompttemplate.example_prompt,
-            prefix=fewshotprompttemplate.prefix,
-            suffix=fewshotprompttemplate.suffix,
-            input_variables=["input", "table_info", "dialect"],
-            example_separator="\n\n"
-        )
-
+    
         local_chain = "I m local chain"
         st.session_state['txt_chain'] = local_chain
         return local_chain
